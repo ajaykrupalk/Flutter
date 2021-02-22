66cb6a8ebc6d75deecac4978f27eee05d31092d1
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2021-02-22 16:52:28 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -11,15 +11,12 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('My first app'), 
         centerTitle: true, 
-        backgroundColor: Colors.indigo[400]
-        ), //AppBar
-      body: Center(
-        child: Image.network('https://images.unsplash.com/photo-1537420327992-d6e192287183?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'),
+        backgroundColor: Colors.indigo[400]), //AppBar
+      body: Padding(
+        padding: EdgeInsets.all(90.0),
+        child: Text('Hello'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {}, 
-        child: Text('Click'), 
-        backgroundColor: Colors.indigo[400]),
+      floatingActionButton: FloatingActionButton(onPressed: () {}, child: Text('Click'), backgroundColor: Colors.indigo[400]),
     ); //Scaffold
   }
 }
