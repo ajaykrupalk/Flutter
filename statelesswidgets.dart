93405dbbac8f64c7e3d859471302ac5598abab93
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2021-02-24 14:29:53 +0530

    Create statelesswidgets.dart

---
+++ statelesswidgets.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: NinjaCard(),
+));
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Ninja ID Card'),
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/studying.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              )
+            ),
+            SizedBox(height:10.0),
+            Text(
+              'Chun Li',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              )
+            ),
+            SizedBox(height:30.0),
+            Text(
+              'Current Level',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              )
+            ),
+            SizedBox(height:10.0),
+            Text(
+              '15',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              )
+            ),
+            SizedBox(height:30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400]
+                ),
+                SizedBox(width:10.0),
+                Text(
+                  'chunli@ninja.co.uk',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
