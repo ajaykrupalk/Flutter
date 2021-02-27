cb70db5a0e50a3437e18ce1cb928711b2c94a39c
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2021-02-27 15:40:04 +0530

    Create menupage.dart

---
+++ Project 1/pages/menupage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MenuPage extends StatelessWidget {
+
+  final Animation<Offset> slideAnimation;
+  final Animation<double> menuAnimation;
+
+  const MenuPage({Key key, this.slideAnimation, this.menuAnimation}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+      return SlideTransition(
+      position: slideAnimation,
+      child: ScaleTransition(
+        scale: menuAnimation,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Dashboard", style: TextStyle(color: Colors.white, fontSize: 22)),
+                SizedBox(height: 10),
+                Text("Messages", style: TextStyle(color: Colors.white, fontSize: 22)),
+                SizedBox(height: 10),
+                Text("Utility Bills", style: TextStyle(color: Colors.white, fontSize: 22)),
+                SizedBox(height: 10),
+                Text("Funds Transfer", style: TextStyle(color: Colors.white, fontSize: 22)),
+                SizedBox(height: 10),
+                Text("Branches", style: TextStyle(color: Colors.white, fontSize: 22)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
