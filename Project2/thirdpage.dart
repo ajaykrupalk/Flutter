36ad8ff2acf587baba40bfae4bf0b6110fb4ff77
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2021-03-02 16:00:51 +0530

    Create thirdpage.dart

---
+++ Project2/thirdpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ThirdRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Active'),
+        centerTitle: true,
+        backgroundColor: Colors.indigo[700],
+      ),
+    );
+  }
+}
