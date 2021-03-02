20d93d97d81fb320f1ef41561dbef0a087263ba7
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2021-03-02 16:01:14 +0530

    Create fourthpage.dart

---
+++ Project2/fourthpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FourthRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recovered'),
+        centerTitle: true,
+        backgroundColor: Colors.indigo[700],
+      ),
+    );
+  }
+}
