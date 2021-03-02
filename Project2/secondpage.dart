4d8deeaae479107029ed9c4897279f4ef6d25159
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2021-03-02 16:00:17 +0530

    Create secondpage.dart

---
+++ Project2/secondpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Confirmed'),
+        centerTitle: true,
+        backgroundColor: Colors.indigo[700],
+      ),
+    );
+  }
+}
