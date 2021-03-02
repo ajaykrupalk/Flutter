ee4c1b951bf0f762d0e744051ba8fd6af7757c1d
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2021-03-02 16:01:34 +0530

    Create fifthpage.dart

---
+++ Project2/fifthpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FifthRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Deceased'),
+        centerTitle: true,
+        backgroundColor: Colors.indigo[700],
+      ),
+    );
+  }
+}
